796b8091e863f3aa9699353d60390b4a85b1ca93
LobbeyTan <tancheelam2@gmail.com>
2023-01-11 11:04:01 +0800

    Added train.sh

---
+++ train.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#SBATCH --partition=cpu-opteron
+#SBATCH --job-name=RL-AI
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --mem=85G
+#SBATCH --qos=normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tancheelam2@gmail.com
+#SBATCH --gres=gpu:1
+#SBATCH --hint=nomultithread
+
+source /home/user/lobbeytan/zelda-ai-game/venv/bin/activate
+
+python main.py
